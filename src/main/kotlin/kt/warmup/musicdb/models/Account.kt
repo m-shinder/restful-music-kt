6e7dc5a1aede98b172ea7492275c04e46a0d03f2
Mihail Shinder <mihail@shinder.ml>
2023-09-29 18:30:00 +0300

    CHANGE: Account now implements UserDetails interface with no authorities

--- src/main/kotlin/kt/warmup/musicdb/models/Account.kt
+++ src/main/kotlin/kt/warmup/musicdb/models/Account.kt
@@ -1,10 +1,12 @@
 package kt.warmup.musicdb.models
 
 import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
 @Table(name = "accounts")
-data class Account(
+data class Account (
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long = 0,
@@ -13,6 +15,19 @@ data class Account(
         var passwordHash: String,
         var hashSalt: String,
         var name: String,
-) {
+): UserDetails {
+        override fun getAuthorities() = mutableListOf<GrantedAuthority>()
+
+        override fun getPassword() = passwordHash
+
+        override fun getUsername() = email
+
+        override fun isAccountNonExpired() = true
+
+        override fun isAccountNonLocked()  = true
+
+        override fun isCredentialsNonExpired() = true
+
+        override fun isEnabled() = true
 
 }
