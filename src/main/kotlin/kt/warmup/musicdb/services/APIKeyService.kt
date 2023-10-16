fa9465eea7cd0ae7513390e8a4c8c2f6066ec3ae
Mihail Shinder <mihail@shinder.ml>
2023-10-16 17:54:43 +0300

    REMOVE: State for API keys. Make JWT the only and single way

--- src/main/kotlin/kt/warmup/musicdb/services/APIKeyService.kt
+++ src/main/kotlin/kt/warmup/musicdb/services/APIKeyService.kt
@@ -1,38 +1,32 @@
 package kt.warmup.musicdb.services
 
 import io.jsonwebtoken.Jwts
-import kt.warmup.musicdb.models.APIKeyType
 import kt.warmup.musicdb.models.Account
-import kt.warmup.musicdb.repos.IAPIkeyRepo
 import org.springframework.stereotype.Service
 import java.sql.Timestamp
 import java.time.Duration
 import java.time.Instant
 
 @Service
-class APIKeyService(
-        val repository: IAPIkeyRepo,
-) {
+class APIKeyService {
     private val jwtSignKey = Jwts.SIG.HS512.key().build()
     private val jwtParser = Jwts.parser().verifyWith(jwtSignKey).build()
 
     fun issueAnonymous(): String {
         return issue(
                 Account(0,"", "", "", "", ""),
-                APIKeyType.ANONYMOUS,
         )
     }
 
     fun issueCreator(creator: Account): String {
         return issue(
                 creator,
-                APIKeyType.CREATOR,
         )
     }
 
     fun issuerOf(token: String) = jwtParser.parseSignedClaims(token).payload.issuer
 
-    private fun issue(issuer: Account, type: APIKeyType): String {
+    private fun issue(issuer: Account): String {
         val iat = Timestamp.from(Instant.now())
         val eat = Timestamp.from(Instant.now().plus(Duration.ofDays(1)))
         val jwt = Jwts.builder()
